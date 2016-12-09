0e720e63f558fa18ebdfa148332213693020c2d9
Parthjhaveri <parthjhaveri209@gmail.com>
2016-12-09 16:09:35 -0500

    js review

--- step-one.js
+++ step-one.js
@@ -1,10 +1,15 @@
+// 1.) ---------------------
+
+//write down mistakes on front of flash cards (logging/writing down mistakes so you can start looking for patterns in mistakes)
 //write a function to console log last value in array
 function logLast(arr) {
-  console.log(arr[arr.length - 1]);
+  //Your code here:
+  console.log(arr[2])
 }
 
 logLast(['dog', 'cat', 'bat']); //logs 'bat'
 
+// 2.) ---------------------
 
 //write a function to console log numbers 0 - 10
 function logTen() {
@@ -15,6 +20,8 @@ function logTen() {
 
 logTen(); //logs numbers 0 - 10
 
+// 3.) ---------------------
+
 //write a function to console log values of an array
 function logArr(arr) {
   for (var i = 0; i < arr.length; i++) {
@@ -24,16 +31,21 @@ function logArr(arr) {
 
 logArr(['dog', 'cat', 'bat']); //logs 'dog', 'cat', 'bat'
 
-//write a function to console log i based on length of array
+
+// 4.) ---------------------
+
 function logArrI(arr) {
   for (var i = 0; i < arr.length; i++) {
     console.log(i);
   }
 }
 
+//LOGS OUT THE INDEX NUMBER FOR EACH ITERATION
+
 logArrI(['dog', 'cat', 'bat']); //logs 0, 1, 2
 
-//write a function to sum numbers 0 - 10
+// 5.) ---------------------
+
 function sumZeroToTen(arr) {
   var sum = 0;
   for (var i = 0; i <= 10; i++) {
@@ -41,21 +53,26 @@ function sumZeroToTen(arr) {
   }
   return sum;
 }
-
+// ONCE IT ADDS A NUMBER, IT STORES THE ITERATION VALUE IN THE SUM. AND FOR EVERY NEXT
+// ITERATION, IT ADDS THE PREVIOUS VALUE ALL THE WAY UP TILL 10 (0+1+2+3+4+5+6+7+8+9+10)
 sumZeroToTen(); //55
 
-//write a function to sum an array
+// 6.) ---------------------
+
 function sumArr(arr) {
   var sum = 0;
   for (var i = 0; i < arr.length; i++) {
-    sum += 0;
+    sum += arr[i]
   }
   return sum;
 }
 
+//IT IS ITERATING THROUGH THE ARRAY AND ADDING THE VALUE OF EVERY I (ARR[I]) TO SUM
+
 sumArr([1, 2, 3, 4, 5]); //15
 
-//write a function to sum i based on length of an array
+// 7.) ---------------------
+
 function sumArrI(arr) {
   var sum = 0;
   for (var i = 0; i < arr.length; i++) {
@@ -64,10 +81,14 @@ function sumArrI(arr) {
   return sum;
 }
 
+//THIS IS TAKING INTO CONSIDERATION THE NUMBER OF INDICES IN THE ARRAY, NOT CONSIDERIN
+// VALUES AT EACH INDEX, AND ITERATING THROUGH TO THE LENGTH OF THE ARRAY
+
 sumArrI([10, 20, 30, 40, 50]); //10
 //(0 + 1 + 2 + 3 + 4, aka the i values)
 
-//if i is greater than the value in the array, log i, otherwise log the array val
+// 8.) ---------------------
+
 function logValOrI(arr) {
   for (var i = 0; i < arr.length; i++) {
     if (i > arr[i]) {
@@ -78,18 +99,25 @@ function logValOrI(arr) {
   }
 }
 
+// ITERATING THROUGH THE ARRAY, AND COMPARING WHETHER THE INDEX IS GREATER THAN THE VALUE.
+// IF THE INDEX IS GREATER THAN THE VALUE, WE PRINT THE INDEX 
+
 logValOrI([10, 5, 3, 1, 0]); //logs: 10, 5, 3, 3, 4
 
-//write a function to log each value in an array, using forEach (do not use a for loop)
+// 9.) ---------------------
+
 function forEachLog(arr) {
   arr.forEach(function(val) {
     console.log(val);
   })
 }
 
+// USES THE ADVANCED ARRAY METHOD OF FOREACH, AND CONSOLE.LOGS EVERY INDEX VALUE
+
 forEachLog(['dog', 'cat', 'bat']); //logs 'dog', 'cat', 'bat'
 
-//write a function to sum each value in an array, using forEach (do not use a for loop)
+// 10.) ---------------------
+
 function forEachSum(arr) {
   var sum = 0;
   arr.forEach(function(val) {
@@ -98,4 +126,27 @@ function forEachSum(arr) {
   return sum;
 }
 
+// THIS LOOPS OVER EVERY INDEX AND BASICALLY TAKES THE VALUE OF EACH INDEX AND ADDS IT TO SUM//, WHICH IS 0.
+
 forEachSum([1, 2, 3, 4, 5]); //15
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- step-two.js
+++ step-two.js
@@ -7,72 +7,84 @@
 //write a function to console log last value in array
 function logLast(arr) {
   //Your code here:
-
+  console.log(arr[arr.length-1])
 }
 
 logLast(['dog', 'cat', 'bat']); //logs 'bat'
 
+// ---------------------------------------------------
 
 //write a function to console log numbers 0 - 10
 function logTen() {
   for(var i = 0; i <= 10; i++) {
     //Your code here:
-
+    console.log(i)
   }
 }
 
 logTen(); //logs numbers 0 - 10
 
+// ---------------------------------------------------
+
 //write a function to console log values of an array
 function logArr(arr) {
   for(var i = 0; i < arr.length; i++) {
     //Your code here:
-
+    console.log(arr[i])
   }
 }
 
 logArr(['dog', 'cat', 'bat']); //logs 'dog', 'cat', 'bat'
 
+
+// ---------------------------------------------------
+
 //write a function to console log i based on length of array
 function logArrI(arr) {
   for(var i = 0; i < arr.length; i++) {
     //Your code here:
-
+    console.log(i)
   }
 }
 
 logArrI(['dog', 'cat', 'bat']); //logs 0, 1, 2
+
+// ---------------------------------------------------
 
 //write a function to sum numbers 0 - 10
 function sumZeroToTen(arr) {
   var sum = 0;
   for(var i = 0; i <= 10; i++) {
     //Your code here:
-
+    sum += i
   }
   return sum;
 }
 
 sumZeroToTen(); //55
 
+// ---------------------------------------------------
+
 //write a function to sum an array
 function sumArr(arr) {
   var sum = 0;
   for(var i = 0; i < arr.length; i++) {
     //Your code here:
-
+    sum += arr[i]
   }
   return sum;
 }
 
 sumArr([1, 2, 3, 4, 5]); //15
 
+// ---------------------------------------------------
+
 //write a function to sum i based on length of an array
 function sumArrI(arr) {
   var sum = 0;
   for(var i = 0; i < arr.length; i++) {
     //Your code here:
-
+    sum += i
   }
   return sum;
 }
@@ -80,34 +92,42 @@ function sumArrI(arr) {
 sumArrI([10, 20, 30, 40, 50]); //10
 //(0 + 1 + 2 + 3 + 4, aka the i values)
 
-//if i is greater than the value in the array, log i, otherwise log the array val
+// ---------------------------------------------------
 function logValOrI(arr) {
-  for(var i = 0; i < arr.length; i++) {
-    //Your code here:
-
+  for (var i = 0; i < arr.length; i++) {
+    if (i > arr[i]) {
+      console.log(i);
+    } else {
+      console.log(arr[i]);
+    }
   }
 }
 
 logValOrI([10, 5, 3, 1, 0]); //logs: 10, 5, 3, 3, 4
 
+// ---------------------------------------------------
+
 //write a function to log each value in an array, using forEach (do not use a for loop)
 function forEachLog(arr) {
   arr.forEach(function(val) {
     //Your code here:
-
+    console.log(val)
   })
 }
 
 forEachLog(['dog', 'cat', 'bat']); //logs 'dog', 'cat', 'bat'
 
+// ---------------------------------------------------
+
 //write a function to sum each value in an array, using forEach (do not use a for loop)
 function forEachSum(arr) {
   var sum = 0;
   arr.forEach(function(val) {
-   //Your code here:
-
+    //Your code here:
+    sum += val
   })
   return sum;
 }
 
 forEachSum([1, 2, 3, 4, 5]); //15
+
